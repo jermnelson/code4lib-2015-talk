721ba14c75d8ebaf5429189835cd30cab38e02c0
Jeremy Nelson <jermnelson.gmail.com>
2015-02-04 10:59:53 -0700

    initial commit

---
+++ talk.py
@@ -0,0 +1,21 @@
+"""
+Created:     2015/02/03
+Copyright:   (c) Jeremy Nelson 2015
+Licence:     GPLv3
+"""
+__author__ = "Jeremy Nelson"
+__license__ = "GPLv3"
+
+from flask import Flask, render_template
+
+talk = Flask(__name__)
+
+@talk.route("/")
+def index():
+    return render_template("index.html")
+
+def main():
+    talk.run(host='0.0.0.0', port=20150, debug=True)
+
+if __name__ == '__main__':
+    main()
